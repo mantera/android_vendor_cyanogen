b3cf3a947ea43ddfad374e45fc98b1a071a2645c
ikarosdev <ikarosdev@gmail.com>
2011-10-02 19:05:57 -0500

    BUMP Triumph

--- products/cyanogen_triumph.mk
+++ products/cyanogen_triumph.mk
@@ -12,7 +12,7 @@ PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := triumph
 PRODUCT_MODEL := Triumph
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=triumph BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=Motorola/Caymus_VRGUS/Triumph/Triumph:2.2.2/FROYO/1010_2_039_110607:user/release-keys PRIVATE_BUILD_DESC="Caymus_VRGUS-user 2.2.2 FROYO 1010_2_039 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=triumph BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=Motorola/Caymus_VRGUS/Triumph/Triumph:2.2.2/FROYO/1010_2_039_110607:user/release-keys PRIVATE_BUILD_DESC="Caymus_VRGUS-user 2.2.2 FROYO 1010_2_039 release-keys"
 
 # Extra Triumph overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/triumph
@@ -26,10 +26,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC1-Triumph
+            ro.modversion=CyanogenMod-7.1.0-Triumph
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC1-Triumph-KANG
+            ro.modversion=CyanogenMod-7.1.0-Triumph-KANG
     endif
 endif
 
